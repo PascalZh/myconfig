da1828db19f69b8284ba81572e7b0f20288b5408
PascalZh <1315521905@qq.com>
2018-05-26 15:14:22 +0800

    add some funny commands recommandations into my myshrc.sh

---
+++ myshrc.sh
@@ -0,0 +1,30 @@
+source myshrc.local.sh
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+alias pls='sudo'
+alias fortune='fortune | lolcat'
+
+# Here are some intereting softwares.
+
+# w3m
+# sudo apt install w3m w3m-img
+
+# sl
+# sudo apt install sl
+
+# fortune
+# sudo apt install fortune-mod fortune-zh
+
+# tree
+# sudo apt install tree
+
+# lolcat
+# usage:
+# man ... | lolcat
+# fortune | lolcat
+# and etc.
+
+# cmatrix
+
+
