bbeb3057860a5621c88dd338ac66abac9e8581e7
PascalZh <1315521905@qq.com>
2020-10-26 22:32:27 +0800

    change vimrc files' name; change plugins; add vmap x

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,167 +7,179 @@ set -x LC_ALL en_US.UTF-8
 #set -x https_proxy https://192.168.31.88:8888
 #set -a -x PATH /usr/local/lib/nodejs/node-v12.16.0-linux-x64/bin
 if status --is-interactive
-    alias ll 'ls -alhF'
-    alias la 'ls -A'
-    alias l 'ls -CF'
-    # -a: --add; -g: --global
-    abbr -a -g pls 'sudo'
-    abbr -a -g rm 'rm -i'
-    abbr -a -g vim 'nvim'
-    abbr -a -g t 'tree -h'
+  alias ll 'ls -alhF'
+  alias la 'ls -A'
+  alias l 'ls -CF'
+  # -a: --add; -g: --global
+  abbr -a -g pls 'sudo'
+  abbr -a -g rm 'rm -i'
+  abbr -a -g vim 'nvim'
+  abbr -a -g t 'tree -h'
 
-    abbr -a -g gs 'git status'
-    abbr -a -g ga 'git add'
-    abbr -a -g gco 'git checkout --cached'
-    abbr -a -g gcm 'git commit -m'
-    abbr -a -g gl 'git lslog'
-    abbr -a -g gd 'git diff'
+  abbr -a -g gs 'git status'
+  abbr -a -g ga 'git add'
+  abbr -a -g gco 'git checkout --cached'
+  abbr -a -g gcm 'git commit -m'
+  abbr -a -g gl 'git lslog'
+  abbr -a -g gd 'git diff'
 
-    abbr -a -g m 'make -j2'
-    abbr -a -g sai 'sudo apt install'
-    abbr -a -g sar 'sudo apt remove'
+  abbr -a -g m 'make -j2'
+  abbr -a -g sai 'sudo apt install'
+  abbr -a -g sar 'sudo apt remove'
 
-    abbr -a -g cff 'nvim ~/.config/fish/config.fish'
-    abbr -a -g cfui 'nvim ~/.config/bspwm/bspwmrc ~/.config/sxhkd/sxhkdrc ~/.compton.conf'
+  abbr -a -g cff 'nvim ~/.config/fish/config.fish'
+  abbr -a -g cfui 'nvim ~/.config/bspwm/bspwmrc ~/.config/sxhkd/sxhkdrc ~/.compton.conf'
 
-    abbr -a -g scf 'source ~/.config/fish/config.fish'
+  abbr -a -g scf 'source ~/.config/fish/config.fish'
 
-    # cool features
-    abbr -a -g fortune 'fortune | lolcat'
+  # cool features
+  abbr -a -g fortune 'fortune | lolcat'
 
-    # various use of particular tasks
-    abbr -a -g create_esp_project 'source ~/bin/_E_create_esp_project_E_'
-    abbr -a -g download_gogh \
-    'wget -O gogh https://git.io/vQgMr && chmod +x gogh'
-    abbr -a -g matlab_ "matlab -nodesktop -nojvm"
-    abbr -a -g leelaz '~/program_files/leela-zero/build/leelaz'
-    abbr -a -g leelaz_ \
-    '~/program_files/leela-zero/build/leelaz -w \
-    ~/program_files/leela-zero/build/best-network \
-    --cpu-only --gtp --noponder -t 4 -p 1'
+  # various use of particular tasks
+  abbr -a -g create_esp_project 'source ~/bin/_E_create_esp_project_E_'
+  abbr -a -g download_gogh \
+  'wget -O gogh https://git.io/vQgMr && chmod +x gogh'
+  abbr -a -g matlab_ "matlab -nodesktop -nojvm"
+  abbr -a -g leelaz '~/program_files/leela-zero/build/leelaz'
+  abbr -a -g leelaz_ \
+  '~/program_files/leela-zero/build/leelaz -w \
+  ~/program_files/leela-zero/build/best-network \
+  --cpu-only --gtp --noponder -t 4 -p 1'
 
 end
 
 function switch_wallpaper
-    python3 -c \
-'
-import argparse
-import os
+  python3 -c \
+  '
+  import argparse
+  import os
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-p", "--period", type=int,
-    help="pass to watch -n PERIOD ...")
-parser.add_argument("-s", "--stop", action="store_true",
-    help="stop the watch -n PERIOD ... process")
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-p", "--period", type=int,
+  help="pass to watch -n PERIOD ...")
+  parser.add_argument("-s", "--stop", action="store_true",
+  help="stop the watch -n PERIOD ... process")
 
-args = parser.parse_args()
+  args = parser.parse_args()
 
-if args.period:
+  if args.period:
     os.system(f"watch -n {args.period} feh --randomize --bg-fill ~/Pictures/wallpaper >/dev/null &")
-elif args.stop:
+    elif args.stop:
     os.system("ps -aux | grep \"watch -n [0-9]* feh --randomize --bg-fill\" | awk \"{print \$2}\" | xargs kill")
 else:
-    os.system("feh --randomize --bg-fill ~/Pictures/wallpaper")
-' $argv
+  os.system("feh --randomize --bg-fill ~/Pictures/wallpaper")
+  ' $argv
 end
 
 function mount_windows_shared_folder
-    sudo mount -t cifs -o username=PascalZh,password=zhang19980918,uid=pascal //192.168.31.82/biyesheji ~/Share
+  sudo mount -t cifs -o username=PascalZh,password=zhang19980918,uid=pascal //192.168.31.82/biyesheji ~/Share
 end
 
 function P_git_clone
 
-    if test (count $argv) -eq 1
-        git clone https://github.com/PascalZh/$argv[1].git
-    else if test (count $argv) -eq 2
-        git clone https://github.com/$argv[1]/$argv[2].git
-    else
-        echo arguments are more than 2!
-    end
+  if test (count $argv) -eq 1
+    git clone https://github.com/PascalZh/$argv[1].git
+  else if test (count $argv) -eq 2
+    git clone https://github.com/$argv[1]/$argv[2].git
+  else
+    echo arguments are more than 2!
+  end
 
 end
 
 function P_install_my_tools
 
-    P_check_installed dialog
-    P_check_installed curl
+  P_check_installed dialog
+  P_check_installed curl
+  P_check_installed python3-pip
 
-    dialog --checklist "Install some common softwares" 0 0 5 \
-    "nvim"            "get newest NVIM nightly(unstable) and put it in /usr/local/bin"         off \
-    "setup_nvim"      "create init.vim; get the plug.vim installed; get all plugins installed" off \
-    "z_lua_for_fish"  "clone z.lua in ~/.local/share and install z.lua for fish"               off \
-    "nodejs"          "show how to install nodejs"                                             off \
-    2> /tmp/dialogtmp
-    if test $status = 0
-        if grep -w "nvim" /tmp/dialogtmp
-            #┌───────────────────────────────────┐
-            #│ download nvim and install plugins │
-            #└───────────────────────────────────┘
-            curl -fLo ~/nvim.appimage https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
-            sudo chmod u+x ~/nvim.appimage
-            sudo mv ~/nvim.appimage /usr/local/bin/nvim
-        end
+  dialog --checklist "Install some common softwares" 0 0 5 \
+  "nvim"            "get newest NVIM nightly(unstable) and put it in /usr/local/bin"         off \
+  "setup_nvim"      "create init.vim; get the plug.vim installed; get all plugins installed" off \
+  "z_lua_for_fish"  "clone z.lua in ~/.local/share and install z.lua for fish"               off \
+  "nodejs"          "show how to install nodejs"                                             off \
+  "gui"             "install bspwm, rofi, zathura, feh, sxhkd, compton"                      off \
+  2> /tmp/dialogtmp
+  if test $status = 0
+    if grep -w "nvim" /tmp/dialogtmp
+      #┌───────────────────────────────────┐
+      #│ download nvim and install plugins │
+      #└───────────────────────────────────┘
+      curl -fLo ~/nvim.appimage https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+      sudo chmod u+x ~/nvim.appimage
+      sudo mv ~/nvim.appimage /usr/local/bin/nvim
+    end
 
-        if grep -w "setup_nvim" /tmp/dialogtmp
-            if test ! -f ~/.config/nvim/init.vim
-                if test ! -d ~/.config/nvim
-                    mkdir ~/.config/nvim
-                end
-                if test ! -d ~/.vim
-                    mkdir -p ~/.vim/autoload
-                end
-
-                echo -e "set runtimepath^=~/.vim runtimepath+=~/.vim/after\nlet &packpath = &runtimepath\nsource ~/.vim/.vimrc" > ~/.config/nvim/init.vim
-                echo "~/.config/nvim/init.vim created"
-
-                curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-                https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-                nvim +PlugInstall +q +q
-                echo -e "\033[33mplug.vim\033[0m installed"
-            else
-                echo -e "\033[33mplug.vim\033[0m already installed; you can install plugins manually"
+    if grep -w "setup_nvim" /tmp/dialogtmp
+      python3 -m pip install --user --upgrade pynvim
+      if test ! -f ~/.config/nvim/init.vim
+        if test ! -d ~/.config/nvim
+          mkdir ~/.config/nvim
             end
-        end
-
-        if grep -w "z_lua_for_fish" /tmp/dialogtmp
-            if test ! -d ~/.config/fish/conf.d
-                mkdir ~/.config/fish/conf.d
+            if test ! -d ~/.vim
+              mkdir -p ~/.vim/autoload
             end
-            P_git_clone skywind3000 z.lua
-            and rm -rf  ~/.local/share/z.lua
-            and mv z.lua ~/.local/share
-            and echo "source (lua ~/.local/share/z.lua/z.lua --init fish | psub)" \
-            > ~/.config/fish/conf.d/z.fish
-        end
 
-        if grep -w "nodejs" /tmp/dialogtmp
-            firefox https://github.com/nodejs/help/wiki/Installation
+            echo -e "set runtimepath^=~/.vim runtimepath+=~/.vim/after\nlet &packpath = &runtimepath\nsource ~/.vim/.vimrc" > ~/.config/nvim/init.vim
+            echo "~/.config/nvim/init.vim created"
+
+            curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+            nvim -u NORC +PlugInstall
+            echo -e "\033[33mplug.vim\033[0m installed"
+        else
+          echo -e "\033[33mplug.vim\033[0m already installed; you can install plugins manually"
         end
     end
 
-    # Appearance
-    P_check_installed redshift-gtk ; P_check_installed feh
-    P_check_installed zathura  ; P_check_installed rofi
-    P_check_installed bspwm    ; P_check_installed sxhkd
-    P_check_installed compton
+    if grep -w "z_lua_for_fish" /tmp/dialogtmp
+      P_check_installed lua5.3
+      if test ! -d ~/.config/fish/conf.d
+        mkdir ~/.config/fish/conf.d
+      end
+      P_git_clone skywind3000 z.lua
+      and rm -rf  ~/.local/share/z.lua
+      and mv z.lua ~/.local/share
+      and echo "source (lua ~/.local/share/z.lua/z.lua --init fish | psub)" \
+      > ~/.config/fish/conf.d/z.fish
+    end
 
-    # Some useful command
-    P_check_installed neofetch
-    P_check_installed ack-grep
-    P_check_installed ncdu
+    if grep -w "nodejs" /tmp/dialogtmp
+       xdg-open https://github.com/nodejs/help/wiki/Installation
+    end
 
-    # C++
+    if grep -w "gui" /tmp/dialogtmp
+      P_check_installed feh
+      P_check_installed zathura  ; P_check_installed rofi
+
+      P_check_installed bspwm    ; P_check_installed sxhkd
+      P_check_installed compton
+    end
+
+  end
+
+  # Some useful command
+  P_check_installed neofetch
+  P_check_installed ack
+  P_check_installed ncdu
+
+  # C++
 end
 
 function P_check_installed
-    set flag (dpkg -s $argv[1] 2> /dev/null)
-    if test -z (count flag)
-        sudo apt install $argv[1]
-    end
+  if test -z $argv[1]
+    return
+  end
+  set info (apt -qq list $argv[1])
+  echo -e "P_check_installed: \033[34mapt -qq list $argv[1]\033[0m ==> $info"
+  if echo $info | grep -v installed > /dev/null
+    echo -e "\033[33mInstalling $argv[1]\033[0m"
+    sudo apt install $argv[1]
+  end
 end
 
 function P_show_cheatsheet
-    echo "# Cheatsheet when I use ubuntu.
+  echo "# Cheatsheet when I use ubuntu.
 ### disable bold font in gnome-terminal, try tab to complete the long code below
 > dconf write /org/gnome/terminal/legacy/profiles:/:a372873d-7fcd-4f50-9566-6aa3c0870ea8/allow-bold  false
 
--- .vim/.vimrc
+++ .vim/.vimrc
@@ -29,18 +29,33 @@ augroup END
 " }}}
 " other settings {{{1
 
+augroup Z_MY_FoldMethod
+  au!
+  "au FileType python setlocal foldmethod=indent foldlevel=99
+  au FileType vim,racket,javascript setlocal foldmethod=marker foldlevel=99 | normal zM
+  au FileType lua setlocal foldexpr=getline(v:lnum)=~'^local\ function'?'a1':(getline(v:lnum)=~'^end'?'s1':'=')
+  au FileType lua setlocal foldmethod=expr
+augroup END
+
 autocmd TermOpen * startinsert
 
 " enable hide the buffer
 set hidden
 
 " always use yanking to paste in other place
-if has('clipboard')
-  if has('unnamedplus')  " When possible use + register for copy-paste
-    set clipboard=unnamed,unnamedplus
-  else         " On mac and Windows, use * register for copy-paste
-    set clipboard=unnamed
-  endif
+if exists('$WSL_DISTRO_NAME')
+  let g:clipboard = {
+        \ 'name': 'win32yank',
+        \ 'copy': {
+        \    '+': 'win32yank.exe -i --crlf',
+        \    '*': 'win32yank.exe -i --crlf',
+        \  },
+        \ 'paste': {
+        \    '+': 'win32yank.exe -o --lf',
+        \    '*': 'win32yank.exe -o --lf',
+        \ },
+        \ 'cache_enabled': 0,
+        \ }
 endif
 
 set timeoutlen=666
@@ -87,8 +102,8 @@ set directory=$HOME/.vim/tmp
 " }}}
 
 " Vim-plug
+command! LightlineToggle call lightline#toggle()
 call plug#begin('~/.vim/bundle')
-
 " Outlooking {{{
 "Plug 'flazz/vim-colorschemes'
 Plug 'itchyny/lightline.vim'
@@ -118,25 +133,20 @@ Plug 'godlygeek/tabular'
 Plug 'junegunn/goyo.vim' | Plug 'junegunn/limelight.vim'
 
 Plug 'terryma/vim-multiple-cursors' | Plug 'terryma/vim-expand-region'
-Plug 'terryma/vim-smooth-scroll'
-Plug 'yuttie/comfortable-motion.vim'
+"Plug 'terryma/vim-smooth-scroll'
+"Plug 'yuttie/comfortable-motion.vim'
 " }}}
 " Other {{{
-Plug 'vim-scripts/utl.vim'
+"Plug 'vim-scripts/utl.vim'
 Plug 'preservim/nerdtree', { 'on':  'NERDTreeToggle' }
-"Plug 'kyazdani42/nvim-web-devicons'
-"Plug 'kyazdani42/nvim-tree.lua'
-
-Plug 'nvim-treesitter/nvim-treesitter'
+"Plug 'zgpio/tree.nvim'
+Plug 'kyazdani42/nvim-web-devicons'
+Plug 'kyazdani42/nvim-tree.lua', { 'on': 'LuaTreeToggle' }
 
 Plug 'mbbill/fencview'
 Plug 'lervag/vimtex', { 'for': 'tex' }
 Plug 'dstein64/vim-startuptime'
-"Plug 'christoomey/vim-tmux-navigator'
-if LINUX()
-  "Plug 'majutsushi/tagbar'
-  Plug 'mileszs/ack.vim'
-endif
+Plug 'mhinz/vim-grepper'
 " }}}
 " Code {{{
 Plug 'derekwyatt/vim-fswitch'
@@ -156,6 +166,8 @@ Plug 'neoclide/coc.nvim', {'branch': 'release'}
 
 Plug 'SirVer/ultisnips'
 Plug 'airblade/vim-gitgutter'
+
+Plug 'nvim-treesitter/nvim-treesitter'
 " }}}
 " Game {{{
 Plug 'johngrib/vim-game-code-break'
@@ -166,10 +178,10 @@ Plug 'ryanoasis/vim-devicons'
 " My own plugin
 Plug 'PascalZh/vim-color-explorer' | Plug 'PascalZh/vim-racket'
 Plug 'PascalZh/vim-badapple', { 'on': 'ZBadApple', 'do': './install.sh' } 
-Plug '~/.vim/bundle/omniwindow.nvim'
+Plug 'PascalZh/omniwindow.nvim'
 call plug#end()
 
 source ~/.vim/.vimrc.bundle
 source ~/.vim/.vimrc.mapping
-source ~/.vim/.vimrc.appearance
-source ~/.vim/.vimrc.language
+source ~/.vim/.vimrc.tui
+source ~/.vim/.vimrc.file_type
